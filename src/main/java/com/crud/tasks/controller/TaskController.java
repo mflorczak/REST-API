8d9d498fef6f838bdcdfae7da8e2f3a3dc9330b4
mflorczak <michal.florczak0@gmail.com>
2018-05-29 11:55:04 +0200

    Module 15.2

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -4,6 +4,7 @@ import com.crud.tasks.domain.TaskDto;
 import com.crud.tasks.mapper.TaskMapper;
 import com.crud.tasks.service.DbService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,8 +24,8 @@ public class TaskController {
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "getTask")
-    public TaskDto getTask(Long id) {
-        return new TaskDto(1L, "test title", "test_content");
+    public TaskDto getTask(@PathVariable("id") Long id) {
+        return taskMapper.mapToTaskDto(service.getTask(id));
     }
 
     @RequestMapping(method = RequestMethod.DELETE, value = "deleteTask")
