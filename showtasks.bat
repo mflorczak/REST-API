cfd0ab394993b14237abbe2512dbac7fe9d20f26
mflorczak <michal.florczak0@gmail.com>
2018-06-07 17:46:15 +0200

    17.4

--- showtasks.bat
+++ showtasks.bat
@@ -1,6 +1,11 @@
 call runcrud
 if "%ERRORLEVEL%" == "0" goto runchrome
 echo Fail open runcrud
+goto fail
 
 :runchrome
 start chrome http://localhost:8080/crud/v1/task/getTasks
+
+:fail
+echo.
+echo There were errors
