f4e0bb5550f9c6839741b84940160967072668a8
mflorczak <michal.florczak0@gmail.com>
2018-06-29 22:25:33 +0200

    Module 19.3

---
+++ src/test/java/com/crud/tasks/service/SimpleEmailServiceTest.java
@@ -0,0 +1,40 @@
+package com.crud.tasks.service;
+
+import com.crud.tasks.domain.Mail;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SimpleEmailServiceTest {
+
+    @InjectMocks
+    private SimpleEmailService simpleEmailService;
+
+    @Mock
+    private JavaMailSender javaMailSender;
+
+    @Test
+    public void shouldSendEmail() {
+        //Given
+        Mail mail = new Mail("test@test.com", "test", "Test message",null);
+        SimpleMailMessage mailMessage = simpleEmailService.createMailMessage(mail);
+
+        //When
+        simpleEmailService.send(mail);
+
+        //Then
+        Mockito.verify(javaMailSender, Mockito.times(1)).send(mailMessage);
+    }
+
+
+}
